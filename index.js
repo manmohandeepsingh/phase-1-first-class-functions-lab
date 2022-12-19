723528bbd8dd946e9b27f850a352e5435dd237a0
Manmohandeep Singh <ltsbcsgo@gmail.com>
2022-12-19 00:09:39 -0500

    commit tree 7cc1516ad2e75b25443d02ea5fd9baef80639032

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Code your solution in this file!
+const returnFirstTwoDrivers = function(scubersDrivers) {
+    return scubersDrivers.slice(0,2);
+}
+
+const returnLastTwoDrivers = function(scubersDrivers) {
+    return scubersDrivers.slice(-2);
+}
+
+const selectingDrivers = [returnFirstTwoDrivers,returnLastTwoDrivers]
+
+function createFareMultiplier(multiplier) {
+   const fareMultiplier = function(fare) {
+    return multiplier * fare;
+   }
+   return fareMultiplier;
+}
+
+const fareDoubler = function(fare) {
+    return createFareMultiplier(fare)(2);
+
+}
+
+const fareTripler = function(fare) {
+    return createFareMultiplier(fare)(3);
+
+}
+
+function selectDifferentDrivers(scubersDrivers, selector) {
+    return selector(scubersDrivers);
+}
